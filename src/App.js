36b59188b37cfecaceeb0db763da16d4edc6d473
FPS man <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-05-15 07:38:55 -0700

    first day done got basic html and wut to do next

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import CustomPart from './AppChild.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <div id="titHeading">
+        <h1 id="theTitle">Kb partpicker<img id="iconPng" src="https://cdn1.iconfinder.com/data/icons/video-game-filled-line/32/switch-mechanical-keyboard-keycap-512.png"></img> </h1>
+      </div>
+      <CustomPart/>
+
+      <div>
+        <button>import</button>
+        <button>export</button>
+        <p>import and export coming soon</p>
+        </div>
     </div>
   );
 }
--- src/AppChild.js
+++ src/AppChild.js
@@ -1,4 +1,55 @@
 import React from 'react';
+import './App.css';
 
 
-class customPart extends React.Component;
+class CustomPart extends React.Component{
+    constructor(props) {
+        super(props);
+        // Don't call this.setState() here!
+        this.state = {
+            case: {},
+            switches:{},
+            keycaps:{},
+            artisans:{},
+            accesories:{}
+        };
+    }
+      
+
+    render(){
+        // TODO: follow this thing down below
+        // im gonna try to  follow the https://www.w3schools.com/react/react_lists.asp 
+        return(
+            <div id="custompart">
+                <div id="case" class="werPart">
+                    <h4>case</h4>
+                    <button>Add Part</button>
+                </div>
+
+                <div id="switches" class="werPart">
+                    <h4>switches</h4>
+                    <button>Add Part</button>
+                </div>
+
+                <div id="keycaps" class="werPart">
+                    <h4>keycaps</h4>
+                    <button>Add Part</button>
+                </div>
+
+                <div id="artisans" class="werPart">
+                    <h4>artisans, hint: try etsy</h4>
+                    <button>Add Part</button>
+                </div>
+
+                <div id="accesories" class="werPart">
+                    <h4>accesories, include: cables,o rings, etc</h4>
+                    <button>Add Part</button>
+                </div>
+                
+            </div>
+            
+        )
+    }
+    
+}
+export default CustomPart;
