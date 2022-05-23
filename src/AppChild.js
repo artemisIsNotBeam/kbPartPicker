fa2e72c59baa1321cbc4e98f1dd5776a1800ee39
FPS man <66878297+artemisIsNotBeam@users.noreply.github.com>
2022-05-23 07:37:20 -0700

    buug fixing didn't work

--- src/AppChild.js
+++ src/AppChild.js
@@ -28,22 +28,23 @@ class CustomPart extends React.Component{
         super(props);
         // Don't call this.setState() here!
         this.state = {
-            case: [{
+            "case": [{
                 "link":"https://kbdfans.com/collections/tofu65/products/fully-assembled-tofu65-black-hot-swap-mechanical-keyboard-with-cement-grey-pbt-dye-sub-keycaps",
                 "name":"tofu 65 assembled",
-                "price":"$200"
+                "price":200
             },{
                 "link":"https://novelkeys.com/collections/top-dogs/products/nk65-tfue-edition",
                 "name":"tfue keycaps",
-                "price":"$80"
+                "price":80
             }],
-            switches:[],
-            keycaps:[],
-            artisans:[],
-            accesories:[],
+            "switches":[],
+            "keycaps":[],
+            "artisans":[],
+            "accesories":[],
         };
 
         this.addThing = this.addThing.bind(this);
+        this.clear = this.clear.bind(this);
     }
       
 
@@ -58,9 +59,29 @@ class CustomPart extends React.Component{
         let tempList = {"link":link,"item":item,"price":price}; 
         //TODO have this line working console.log(this.state[whichList]);
 
-        this.setState({case: tempList});
+        this.setState({whichList: [...tempList]});
+        // https://stackoverflow.com/questions/29886552/why-are-objects-not-iterable-in-javascript look at this to add object into state
 
-        console.log(this.state.case)
+        //  okay I think to by pass I can just run it as a list:[(da link here),(name be here),(price)]
+        console.log(this.state.whichList)
+    }
+
+    clear(whichList){
+        this.setState({whichList:null})
+        console.log(this.state.whichList)
+    }
+
+    totPrice(DaState){
+        let goThrough = ["case","switches","keycaps","artisans","accesories"]
+        let price = 0;
+
+        goThrough.map((place) =>{
+            let newPlace = DaState[place];
+            newPlace.map((kbPart) =>{
+                price += kbPart["price"]
+            })
+        });
+        return ""+price
     }
 
     
@@ -75,9 +96,8 @@ class CustomPart extends React.Component{
                 <div id="case" class="werPart">
                     <h4>case</h4>
                     {this.state.case.map((part) => <Things list={part}/>)}
-
-                    <button>Clear</button>
-                    <button onClick={() => this.addThing(0)}>Add Part</button>
+                    <button onClick={()=> this.clear("case")}>Clear</button>
+                    <button onClick={() => this.addThing("case")}>Add Part</button>
                 </div>
 
                 <div id="switches" class="werPart">
@@ -103,7 +123,7 @@ class CustomPart extends React.Component{
                     <button>Clear</button>
                     <button>Add Part</button>
                 </div>
-                <p>Price</p>
+                <p>Price: {this.totPrice(this.state)}</p><button>Button price tester</button>
             </div>
             
         )
